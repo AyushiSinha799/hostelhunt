0b0975c041e03ad127cb936b65013446e9758eb7
AyushiSinha799 <116423338+AyushiSinha799@users.noreply.github.com>
2023-10-28 10:31:17 +0530

    initial commit

---
+++ src/main/java/Com/SERVELET/LoginServelet.java
@@ -0,0 +1,39 @@
+package Com.SERVELET;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Com.entity.User;
+
+@WebServlet("/login")
+public class LoginServelet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		try {
+
+			String em = req.getParameter("email");
+			String ps = req.getParameter("password");
+			User u =new User();
+			HttpSession session=req.getSession();
+
+			if ("admin@gmail.com".equals(em) && "admin@121".equals(ps)) {
+				session.setAttribute("userobj", u);
+				u.setRole("admin");
+				resp.sendRedirect("admin.jsp");
+			} else {
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
